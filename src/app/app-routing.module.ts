78f7faae44c492f65a93927e628317f39c3bbd0c
Nagarajan Sivaraman <naga4042@gmail.com>
2019-04-09 14:41:36 +0530

    refactoring of pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {HackathonComponent} from './hackathon/hackathon.component';
-import {BetaprogramsComponent} from './betaprograms/betaprograms.component';
+import {HomeComponent} from './pages/home/home.component';
+import {HackathonComponent} from './pages/hackathon/hackathon.component';
+import {BetaprogramsComponent} from './pages/betaprograms/betaprograms.component';
+import {LoginComponent} from './pages/login/login.component';
+import {RegisterComponent} from './pages/register/register.component';
 
 const routes: Routes = [
   {
@@ -10,12 +12,20 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
-    path: 'hackathon',
+    path: 'hackathons',
     component: HackathonComponent
   },
   {
     path: 'betaprograms',
     component: BetaprogramsComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { HackathonComponent } from './hackathon/hackathon.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { BetaprogramsComponent } from './betaprograms/betaprograms.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HackathonComponent } from './pages/hackathon/hackathon.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BetaprogramsComponent } from './pages/betaprograms/betaprograms.component';
 
 @NgModule({
   declarations: [
